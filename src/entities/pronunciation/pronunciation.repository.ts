c1dc85191ea25b847e4f372f351568951b1db676
Jose Albizures <albizures3601@gmail.com>
2019-11-05 19:52:27 +0300

    expect a word entity to create a new pronunciation

--- src/entities/pronunciation/pronunciation.repository.ts
+++ src/entities/pronunciation/pronunciation.repository.ts
@@ -2,10 +2,12 @@ import { getRepository } from 'typeorm';
 import { Pronunciation, FilterPronunciation } from './pronunciation.entity';
 import { createOptional, SimpleLike } from '../../utils';
 import { User } from '../user/user.entity';
+import { Word } from '../word/word.entity';
 
 interface NewPronunciation {
   file: string;
   user: User;
+  word: Word;
 }
 
 const create = (pronunciation: NewPronunciation): Promise<Pronunciation> => {
